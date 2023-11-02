8d596c42c6f7c30dee65cc3bcffdbda5a3f59a05
dustinlaa <drl3@njit.edu>
2023-11-02 11:48:53 -0400

    cleaning

--- splitter.py
+++ splitter.py
@@ -1,13 +1,16 @@
 # pip install demucs
 import subprocess
+# import demucs.separate
 
 # input_audio_path = input("Please specify the input file: ")
 
 # if input_audio_path == "":
 #     input_audio_path = input("Please specify the input file: ")
 
-input_audio_path = "stay_qUgdaiNx.wav"
+input_audio_path = "insideOut.wav"
 output_dir = "/stem-output"
+
+# !python -m demucs.separate /content/stay_qUgdaiNx.wav <-- google colab
 
 # Construct the command as a list of arguments
 command = ["python", "-m", "demucs.separate", input_audio_path]
